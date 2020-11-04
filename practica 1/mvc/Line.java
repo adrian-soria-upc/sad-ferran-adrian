221e5ed49faeef9e99c7f5ea918b5ba86f66b891
adrian-soria-upc <71636181+adrian-soria-upc@users.noreply.github.com>
2020-11-04 15:41:10 +0100

    Add files via upload

---
+++ practica 1/mvc/Line.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Line {
+  private int cursor;
+  private StringBuilder stringLine;
+  private boolean insert;
+
+  public Line(){
+ 	  stringLine = new StringBuilder();
+  }
+
+  public void addChar(char in){
+  	stringLine.insert(cursor,in);
+  	cursor++;
+  }
+
+  public void home(){
+      cursor = 0;
+  }
+
+  public void end(){
+ 	cursor = stringLine.length();
+  }
+
+  public void delete(){
+	  if(cursor < stringLine.length() - 2){
+	  	stringLine.deleteCharAt(cursor);
+	  }
+  }
+
+  public void moveCursor(int move){
+        //move indica amb +1 o -1 si va a l'esquerra o a la dreta
+        //SEGUR QUE HI HA UNA MILLOR SOLUCIÓ
+        if (cursor + move >= 0 && cursor + move <= stringLine.length()){
+            cursor += move;
+        }
+  }
+
+  public void backspace(){
+  	if(cursor != 0){
+	  	stringLine.deleteCharAt(cursor - 1);
+		cursor--;
+	}
+  }
+
+  public void insert(){
+    insert = !insert;
+  }
+
+  public String displayString(){
+    StringBuilder displayString = new StringBuilder();
+    displayString.append('\r');
+    displayString.append(stringLine.toString());
+    displayString.append(" ");
+    displayString.append("\033[");
+    displayString.append(1 + stringLine.length() - cursor);
+    displayString.append("D");
+    return displayString.toString();
+  }
+
+  public String toString() {
+      //StringBuilder builder = new StringBuilder(stringLine.size());
+    	//for(Character ch: stringLine){
+      //  	builder.append(ch);
+    	//}
+    	//return builder.toString();
+    return stringLine.toString();
+  }
+}
