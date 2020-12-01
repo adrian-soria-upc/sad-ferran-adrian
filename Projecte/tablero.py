48c10cb48331c2fe2c79633b72eae354103ce1dc
adrian-soria-upc <71636181+adrian-soria-upc@users.noreply.github.com>
2020-12-01 20:36:18 +0100

    Add files via upload

--- Projecte/tablero.py
+++ Projecte/tablero.py
@@ -5,49 +5,49 @@ from piezas.caballo import Caballo
 from piezas.king import King
 from piezas.queen import Queen
 
-def crearMatriz(color):
-        M= [[0 for x in range(8)] for _ in range(8)] #Mesa
-        jugando = True
-
-        M[0][0] = Torre(0, 0, "R")
-        M[0][1] = Caballo(0, 1, "R")
-        M[0][2] = Alfil(0, 2, "R")
-        M[0][3] = Queen(0, 3, "R")
-        M[0][4] = King(0, 4, "R")
-        M[0][5] = Alfil(0, 5, "R")
-        M[0][6] = Caballo(0, 6, "R")
-        M[0][7] = Torre(0, 7, "R")
-
-        M[1][0] = Peon(1, 0, "R")
-        M[1][1] = Peon(1, 1, "R")
-        M[1][2] = Peon(1, 2, "R")
-        M[1][3] = Peon(1, 3, "R")
-        M[1][4] = Peon(1, 4, "R")
-        M[1][5] = Peon(1, 5, "R")
-        M[1][6] = Peon(1, 6, "R")
-        M[1][7] = Peon(1, 7, "R")
+class Tablero():
+    def __init__(self):
+        self.M= [[0 for x in range(8)] for _ in range(8)] #Mesa
+        self.jugando = True
+    
+        self.M[0][0] = Torre(0, 0, "R")
+        self.M[0][1] = Caballo(0, 1, "R")
+        self.M[0][2] = Alfil(0, 2, "R")
+        self.M[0][3] = Queen(0, 3, "R")
+        self.M[0][4] = King(0, 4, "R")
+        self.M[0][5] = Alfil(0, 5, "R")
+        self.M[0][6] = Caballo(0, 6, "R")
+        self.M[0][7] = Torre(0, 7, "R")
+    
+        self.M[1][0] = Peon(1, 0, "R")
+        self.M[1][1] = Peon(1, 1, "R")
+        self.M[1][2] = Peon(1, 2, "R")
+        self.M[1][3] = Peon(1, 3, "R")
+        self.M[1][4] = Peon(1, 4, "R")
+        self.M[1][5] = Peon(1, 5, "R")
+        self.M[1][6] = Peon(1, 6, "R")
+        self.M[1][7] = Peon(1, 7, "R")
         
-        M[6][0] = Peon(6, 0, "A")
-        M[6][1] = Peon(6, 1, "A")
-        M[6][2] = Peon(6, 2, "A")
-        M[6][3] = Peon(6, 3, "A")
-        M[6][4] = Peon(6, 4, "A")
-        M[6][5] = Peon(6, 5, "A")
-        M[6][6] = Peon(6, 6, "A")
-        M[6][7] = Peon(6, 7, "A")
-
-        M[7][0] = Torre(7, 0, "A")
-        M[7][1] = Caballo(7, 1, "A")
-        M[7][2] = Alfil(7, 2, "A")
-        M[7][3] = Queen(7, 3, "A")
-        M[7][4] = King(7, 4, "A")
-        M[7][5] = Alfil(7, 5, "A")
-        M[7][6] = Caballo(7, 6, "A")
-        M[7][7] = Torre(7, 7, "A")
+        self.M[6][0] = Peon(6, 0, "A")
+        self.M[6][1] = Peon(6, 1, "A")
+        self.M[6][2] = Peon(6, 2, "A")
+        self.M[6][3] = Peon(6, 3, "A")
+        self.M[6][4] = Peon(6, 4, "A")
+        self.M[6][5] = Peon(6, 5, "A")
+        self.M[6][6] = Peon(6, 6, "A")
+        self.M[6][7] = Peon(6, 7, "A")
+    
+        self.M[7][0] = Torre(7, 0, "A")
+        self.M[7][1] = Caballo(7, 1, "A")
+        self.M[7][2] = Alfil(7, 2, "A")
+        self.M[7][3] = Queen(7, 3, "A")
+        self.M[7][4] = King(7, 4, "A")
+        self.M[7][5] = Alfil(7, 5, "A")
+        self.M[7][6] = Caballo(7, 6, "A")
+        self.M[7][7] = Torre(7, 7, "A")
         
-        return dibujarMesaString(M, color)
-
-def dibujarMesaString(matriz, jugador):
+    
+    def dibujarMesaString(self, matriz, jugador):
         mesa = ""
         if jugador == "azul":
             mesa += "\033[;36m"+"                 TURNO JUGADOR AZUL" + "\n"
@@ -64,25 +64,57 @@ def dibujarMesaString(matriz, jugador):
             mesa += ']' + "\n" + "   -------------------------------------------------" + "\n"
         mesa += "       A     B     C     D     E     F     G     H" + "\n"
         return mesa
-        
-def actualizartablero(turno, color):
-	if turno==0:
-		return crearMatriz(color)
-	elif turno<4:
-		return "jugada\n"
-	else:#esta mal
-		if color=="azul":
-			return "\nganador rojo\n"
-		else:
-			return "\nganador azul\n"
-def myTurn(turno, color):
-	if ((turno % 2 != 0) & (color == 'rojo')): #truno blancas (IMPAR)
-		return True
-	elif ((turno % 2 == 0) & (color == 'azul')): #turno negras (PAR)
-		return True
-	else:	
-		return False	
-
-	
-def comandocorrecto(turno, color, line):
-	return True	
+            
+    def actualizartablero(self, turno, color):
+    	if turno==0:
+    		return self.dibujarMesaString(self.M, color)
+    	elif turno<4:
+    		return self.dibujarMesaString(self.M, color)
+    	else:#esta mal
+    		if color=="azul":
+    			return "\nganador rojo\n"
+    		else:
+    			return "\nganador azul\n"
+    def myTurn(turno, color):
+    	if ((turno % 2 != 0) & (color == 'rojo')): #truno blancas (IMPAR)
+    		return True
+    	elif ((turno % 2 == 0) & (color == 'azul')): #turno negras (PAR)
+    		return True
+    	else:	
+    		return False	
+     
+    def comandocorrecto(self, turno, color, inp):
+        pos = []
+        #Servidor procesa jugada
+        pos.append(int(inp[0]) - 1)
+        pos.append(ord(inp[1]) - ord("A"))
+        pos.append(int(inp[2]) - 1)
+        pos.append(ord(inp[3]) - ord("A"))
+        return self.controlMovimiento(pos, color)
+    
+    def controlMovimiento(self, pos, color):
+            for i in pos:
+                if i > 7 or i < 0: #Miramos si estamos dentro del rango de actuación
+                    return False
+            if self.M[pos[0]][pos[1]].equipo == "R" and color == "rojo":
+                return False
+            elif self.M[pos[0]][pos[1]].equipo == "A" and color == "azul":
+                return False
+            elif self.M[pos[2]][pos[3]] == 0: #Miramos si la casilla esta vacía
+                if self.move(pos):
+                    return True
+            elif self.M[pos[2]][pos[3]].equipo == self.M[pos[0]][pos[1]].equipo: #Miramos si la pieza de destino no es de nuestro equipo
+                return False
+            else:
+                if self.move(pos):
+                    return True
+    
+    def move(self,pos):
+        mcomp = self.M[pos[0]][pos[1]].valid_move(self.M, pos)
+        if mcomp:
+             self.M[pos[2]][pos[3]] = self.M[pos[0]][pos[1]]
+             self.M[pos[0]][pos[1]] = 0
+             return True
+        else:
+            return True #Ha de posar false
+    
