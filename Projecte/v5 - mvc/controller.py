efd8644e32b794bffd10d3b6d78b5ef2bfe2141c
adrian-soria-upc <71636181+adrian-soria-upc@users.noreply.github.com>
2020-12-06 20:31:22 +0100

    Update controller.py

--- Projecte/v5 - mvc/controller.py
+++ Projecte/v5 - mvc/controller.py
@@ -1,53 +1,53 @@
 from piezas.pieza import Pieza
-def comandoCorrecto(color, line, m):
+import server
+#intToString() falta per fer
+def comandoCorrecto(color, line):
     pos = [] 
     line = line.upper()
     if line[len(line) - 3] == "F" and line[len(line) - 2] == "F":
-        tumbarRey(color, m)
+        tumbarRey(color)
         return True
     pos.append(ord(line[len(line) - 5]) - ord("1"))
     pos.append(ord(line[len(line) - 4]) - ord("A"))
     pos.append(ord(line[len(line) - 3]) - ord("1"))
     pos.append(ord(line[len(line) - 2]) - ord("A"))
-    print(pos)
-    return controlMovimiento(pos, color, m)
+    return controlMovimiento(pos, color)
 
-def tumbarRey(color, m):
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            if m[i][j].equipo != "N":
-                if m[i][j].tipo == "K" and m[i][j].equipo == color:
-                    m[i][j] = Pieza(" ", "N", i, j)
+def tumbarRey(color):
+    for i in range(len(server.tablero.M)):
+        for j in range(len(server.tablero.M[i])):
+            if server.tablero.M[i][j].tipo == "K" and server.tablero.M[i][j].equipo == color:
+                server.tablero.M[i][j] = Pieza(" ", "N", i, j)
 
-#Optimitzar al final
-def controlMovimiento(pos, color, m):
+def controlMovimiento(pos, color):
         for i in pos:
             if i > 7 or i < 0: #Miramos si estamos dentro del rango de actuación
                 return False
-        if m[pos[0]][pos[1]].equipo != color or m[pos[2]][pos[3]].equipo == m[pos[0]][pos[1]].equipo:
+        if server.tablero.M[pos[0]][pos[1]].equipo != color or server.tablero.M[pos[2]][pos[3]].equipo == server.tablero.M[pos[0]][pos[1]].equipo:
             return False
-        elif m[pos[0]][pos[1]].valid_move(m, pos):
-            return move(pos, m)
+        elif server.tablero.M[pos[0]][pos[1]].valid_move(server.tablero.M, pos):
+            return move(pos)
         else:
             return False
 
-def enroque(pos, m):
+def enroque(pos):
     c = pos[3] - pos[1]
     if c > 0:
-        m[pos[2]][pos[3]] = m[pos[0]][pos[1]]
-        m[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
-        m[pos[2]][pos[3] - 1] = m[pos[2]][pos[3] + 1]
-        m[pos[2]][pos[3] + 1] = Pieza(" ", "N", pos[2], pos[3] + 1)
+        server.tablero.M[pos[2]][pos[3]] = server.tablero.M[pos[0]][pos[1]]
+        server.tablero.M[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
+        server.tablero.M[pos[2]][pos[3] - 1] = server.tablero.M[pos[2]][pos[3] + 1]
+        server.tablero.M[pos[2]][pos[3] + 1] = Pieza(" ", "N", pos[2], pos[3] + 1)
     else:
-        m[pos[2]][pos[3]] = m[pos[0]][pos[1]]
-        m[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
-        m[pos[2]][pos[3] + 1] = m[pos[2]][pos[3] - 2]
-        m[pos[2]][pos[3] - 2] = Pieza(" ", "N", pos[2], pos[3] - 2)
+        server.tablero.M[pos[2]][pos[3]] = server.tablero.M[pos[0]][pos[1]]
+        server.tablero.M[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
+        server.tablero.M[pos[2]][pos[3] + 1] = server.tablero.M[pos[2]][pos[3] - 2]
+        server.tablero.M[pos[2]][pos[3] - 2] = Pieza(" ", "N", pos[2], pos[3] - 2)
 
-def move(pos, m):
-    if m[pos[0]][pos[1]].enroque:
-        enroque(pos, m)
+def move(pos):
+    if server.tablero.M[pos[0]][pos[1]].enroque:
+        enroque(pos)
     else:
-        m[pos[2]][pos[3]] = m[pos[0]][pos[1]]
-        m[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
+        server.tablero.M[pos[2]][pos[3]] = server.tablero.M[pos[0]][pos[1]]
+        server.tablero.M[pos[0]][pos[1]] = Pieza(" ", "N", pos[0], pos[1])
+    server.tablero.jugador = abs(server.tablero.jugador - 1)
     return True 
