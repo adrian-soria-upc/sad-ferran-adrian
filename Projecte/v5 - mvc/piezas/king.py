3c4d2bac9e6a31dfab211c45eddf864a24c93b52
adrian-soria-upc <71636181+adrian-soria-upc@users.noreply.github.com>
2020-12-06 20:44:31 +0100

    Update king.py

--- Projecte/v5 - mvc/piezas/king.py
+++ Projecte/v5 - mvc/piezas/king.py
@@ -11,20 +11,18 @@ class King(Pieza):
         mpos = True
         if abs(f) > 1 or abs(c) > 1:
             #Enroque
-            if pos[0] == self.fila and pos[1] == self.col:
+            if pos[0] == self.fila and pos[1] == self.col:#NO PASSAR FILA I COLUMNA 
                 if f == 0:
                     if c == -2:
                         if mesa[pos[0]][pos[1] - 1].equipo == "N" and  mesa[pos[0]][pos[1] - 2].equipo == "N" and mesa[pos[0]][pos[1] - 3].equipo == "N":
-                            if mesa[pos[0]][pos[1] - 4].equipo != "N":
-                                if mesa[pos[0]][pos[1] - 4].tipo == "T" and mesa[pos[0]][pos[1] - 4].equipo == self.equipo:
-                                    mpos = True
-                                    self.enroque = True
+                            if mesa[pos[0]][pos[1] - 4].tipo == "T" and mesa[pos[0]][pos[1] - 4].equipo == self.equipo:
+                                mpos = True
+                                self.enroque = True
                     elif c == 2:
                         if mesa[pos[0]][pos[1] + 1].equipo == "N" and  mesa[pos[0]][pos[1] + 2].equipo == "N":
-                            if mesa[pos[0]][pos[1] + 3].equipo != "N":
-                                if mesa[pos[0]][pos[1] + 3].tipo == "T" and mesa[pos[0]][pos[1] + 3].equipo == self.equipo:
-                                    mpos = True
-                                    self.enroque = True
+                            if mesa[pos[0]][pos[1] + 3].tipo == "T" and mesa[pos[0]][pos[1] + 3].equipo == self.equipo:
+                                mpos = True
+                                self.enroque = True
                     else:
                         mpos = False
                         self.enroque = False
