99a0572d0f3a8f91ee2bf5e7095c409599109480
adrian-soria-upc <adrian.soria@estudiantat.upc.edu>
2020-12-08 14:27:46 +0100

    Correcció error Peón es pot moure dues caselles en direcció contraria

--- Projecte/v6/piezas/peon.py
+++ Projecte/v6/piezas/peon.py
@@ -9,7 +9,11 @@ class Peon(Pieza):
         f = pos[2] - pos[0] #Fila final - Fila inicial
         c = pos[3] - pos[1] #Columna final - Columna inicial
         mpos = True
-        if abs(f) > 1 or abs(c) > 1:
+        if self.equipo == 0 and f <= 0: #Las rojas solo se pueden mover hacia abajo (filas positivas)
+            mpos = False
+        elif self.equipo == 1 and f >= 0: #Las azules solo se pueden mover hacia arriba (filas negativas)
+            mpos = False
+        elif abs(f) > 1 or abs(c) > 1:
             if pos[0] == 1 or pos[0] == 6:
                 if abs(f) == 2:
                     if self.equipo == 1 and mesa[pos[0] - 1][pos[1]].equipo == "N":#Mirar filera seguent
@@ -22,10 +26,6 @@ class Peon(Pieza):
                         mpos = False      
             else:
                 mpos = False
-        elif self.equipo == 0 and f <= 0: #Las rojas solo se pueden mover hacia abajo (filas positivas)
-            mpos = False
-        elif self.equipo == 1 and f >= 0: #Las azules solo se pueden mover hacia arriba (filas negativas)
-            mpos = False
         else:
             if pos[3] == pos[1]:
                 mpos = True
