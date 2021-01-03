2423b397f931bd978f598d7ed50c88471187a0e2
adrian-soria-upc <adrian.soria@estudiantat.upc.edu>
2021-01-03 17:21:58 +0100

    PR3

--- Practica3/Client/UserInterface.java
+++ Practica3/Client/UserInterface.java
@@ -10,21 +10,34 @@ public class UserInterface extends JFrame{
     public UserInterface(){
         DefaultListModel<String> messages = new DefaultListModel<>();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);//800 600
+        setSize(800, 600);
 
         setLayout(new GridBagLayout());
 
+        JLabel title = new JLabel("Welcome, you can start!");
+        title.setFont(new Font("Titulo", Font.BOLD, 26));
+        add(title, genGrid(0, 0, 1, 1));
+
+        //JButton startButton = new JButton("Start");
+        //startButton.addActionListener(new ActionListener() {
+        //    @Override
+        //    public void actionPerformed(ActionEvent arg0) {
+        //        title.setText("Connexion established");
+        //        remove(startButton);
+        //    }
+        //});
+        //add(startButton, genGrid(0, 1, 4, 1));
+
         JScrollPane scrollPane = new JScrollPane();
         scrollPane.setVerticalScrollBarPolicy(22);
-        Dimension preferredSize = new Dimension(334,150);
+        Dimension preferredSize = new Dimension(600,400);
         scrollPane.setPreferredSize(preferredSize);
         JList<String> messageList = new JList<>(messages);
         scrollPane.setViewportView(messageList);
-        add(scrollPane, genGrid(0, 1, 1, 1)); //Mostra l'historial del chat
-        
-
+        add(scrollPane, genGrid(0, 2, 1, 1)); //Mostra l'historial del chat
+       
         JTextField messageTextField = new JTextField(30);
-        add(messageTextField, genGrid(0, 2, 4, 1));
+        add(messageTextField, genGrid(0, 3, 4, 1));
 
         JButton sendMessageButton = new JButton("Send message");
         sendMessageButton.addActionListener(new ActionListener() {
@@ -36,7 +49,8 @@ public class UserInterface extends JFrame{
                 messages.addElement(message);
             }
         });
-        add(sendMessageButton, genGrid(0, 3, 4, 1));
+        add(sendMessageButton, genGrid(0, 4, 4, 1));
+
         setVisible(true);
     }
 
